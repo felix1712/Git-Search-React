7b7d09daf3e8e2976e0f328f958aad7712ef0118
unknown <felix.andrean17@gmail.com>
2022-11-30 23:20:19 +0700

    feature: add service context and env

---
+++ src/Contexts/ServiceContext.tsx
@@ -0,0 +1,76 @@
+import React, { IChildrenOnly, IServiceActionsContext } from "react";
+import axios, { AxiosRequestConfig, CancelToken } from "axios";
+
+export const ServiceActionsContext = React.createContext({} as IServiceActionsContext);
+
+const ServiceProvider = (props: IChildrenOnly) => {
+  const { children } = props;
+
+  const endpointUrl = (query: string) => {
+    const endpoint = process.env.API_URL;
+    return endpoint + query;
+  };
+
+  const getAxios = async (
+    query: string,
+    cancelToken: CancelToken,
+    arrayBuffer?: string
+  ) => {
+    let config: AxiosRequestConfig = {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      cancelToken
+    };
+
+    if (!!arrayBuffer) config.responseType = "arraybuffer";
+
+    const combinedUrl = endpointUrl(query)!;
+
+    try {
+      const response = await axios.get(combinedUrl, config);
+
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const postAxios = async (
+    query: string,
+    payload: any,
+    cancelToken: CancelToken
+  ) => {
+    const config: AxiosRequestConfig = {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      cancelToken
+    };
+
+    if (!payload) payload = {};
+
+    const combinedUrl = endpointUrl(query)!;
+
+    try {
+      const response = await axios.post(combinedUrl, payload, config);
+
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const serviceActionsValue = {
+    getAxios,
+    postAxios
+  };
+
+  return (
+    <ServiceActionsContext.Provider value={serviceActionsValue}>
+      {children}
+    </ServiceActionsContext.Provider>
+  );
+};
+
+export default ServiceProvider;
