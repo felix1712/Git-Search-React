67d633f098860a2caf42a711e1b293c11fd318b0
unknown <felix.andrean17@gmail.com>
2022-12-03 23:53:27 +0700

    feature: add watchers, code and fork also maintain responsive

--- src/components/SearchRepository/SearchRepository.tsx
+++ src/components/SearchRepository/SearchRepository.tsx
@@ -1,9 +1,12 @@
-import {ISearchRepositoryProps, IUseSearchRepo} from 'react';
+import {ISearchRepositoryProps, IUseSearchRepoValue} from 'react';
 import { useSearchRepository } from './useSearchRepository';
 import repo from "../../assets/icons/repo.png";
 import star from "../../assets/icons/star.png";
 import code from "../../assets/icons/code.png";
+import watch from "../../assets/icons/eye.png";
+import fork from "../../assets/icons/fork.png";
 import { Text } from '../Text/Text';
+import { ImageTag } from '../ImageTag/ImageTag';
 import { redirectGithub } from '../../utils/SharedFunction';
 
 import styles from './SearchRepository.module.css'
@@ -11,14 +14,15 @@ import styles from './SearchRepository.module.css'
 export const SearchRepository = (props: ISearchRepositoryProps) => {
   const {
     searchData,
-  }: IUseSearchRepo = useSearchRepository(props);
+    searchTopic
+  }: IUseSearchRepoValue = useSearchRepository(props);
   
   return (
     <div className={styles["repo_frame"]}>
       {searchData.map((data: any, index: number) => (
         <div className={styles["repo_container"]} key={data.name + index}>
-          <div className={styles["image_repo"]}><img src={repo} alt="repo_icon"/></div>
-          <div>
+          <div className={styles["image_repo"]}><ImageTag src={repo} alt="repo_icon"/></div>
+          <div className={styles["repo_item"]}>
             <div className={styles["repo_name_container"]} onClick={() => redirectGithub(data.html_url)}>
               <Text><b>{data.full_name}</b></Text>
             </div>
@@ -27,16 +31,24 @@ export const SearchRepository = (props: ISearchRepositoryProps) => {
             </div>
             <div className={styles["repo_topics_container"]}>
               {data.topics?.slice(0, 5).map((items: string) => (
-                <span>{items}</span>
+                <span onClick={() => searchTopic(items)}>{items}</span>
               ))}
             </div>
             <div className={styles["repo_etc_container"]}>
               <span>
-                <img src={star} alt="star" />
+                <ImageTag src={star} alt="star" />
                 <Text>{data.stargazers_count}</Text>
               </span>
               <span>
-                <img src={code} alt="language" />
+                <ImageTag src={fork} alt="star" />
+                <Text>{data.forks_count}</Text>
+              </span>
+              <span>
+                <ImageTag src={watch} alt="star" />
+                <Text>{data.watchers_count}</Text>
+              </span>
+              <span>
+                <ImageTag src={code} alt="language" />
                 <Text>{data.language}</Text>
               </span>
               
--- src/components/SearchRepository/useSearchRepository.ts
+++ src/components/SearchRepository/useSearchRepository.ts
@@ -1,21 +1,34 @@
-import {useState, useEffect, useContext, ISearchRepositoryProps, IUseSearchRepo} from 'react';
+import { useEffect, useContext, ISearchRepositoryProps, IUseSearchRepoValue } from 'react';
+import { useNavigate, createSearchParams } from 'react-router-dom'
 import { SearchContext } from '../../Contexts/SearchContext';
 import { SearchActionsContext } from "../../Contexts/SearchContext";
 
 export const useSearchRepository = (props: ISearchRepositoryProps) => {
   const {q, rowDisplay, pageDisplay} = props
+  const navigate = useNavigate();
   const {searchData} = useContext(SearchContext);
   const { searchGlobal } = useContext(
     SearchActionsContext
   );
-  const query = `repositories?q=${q}&per_page=${rowDisplay}&page=${pageDisplay}`;
+  const query = `search/repositories?q=${q}&per_page=${rowDisplay}&page=${pageDisplay}`;
   
+  const searchTopic = (q: string) => {
+    const paramsUrl = `?type=topics&q=${q}`;
+    const topicQuery = `search/topics?q=${q}&per_page=${rowDisplay}&page=${pageDisplay}`;
+    searchGlobal(topicQuery)
+    navigate({
+      pathname: '/search',
+      search: `?${createSearchParams(paramsUrl)}`,
+    });
+  }
+
   useEffect(() => {
     searchGlobal(query)
   }, [rowDisplay, pageDisplay])
 
-  const searchPreviewValue: IUseSearchRepo = {
+  const searchPreviewValue: IUseSearchRepoValue = {
     searchData,
+    searchTopic
   }
   return searchPreviewValue;
 }
