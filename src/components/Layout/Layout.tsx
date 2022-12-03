8be330650a3760790031044c9ed50af49ec82af6
unknown <felix.andrean17@gmail.com>
2022-12-03 23:52:36 +0700

    feature: add layout path

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense, useRef } from "react";
 import { Navigate, Routes, Route} from "react-router-dom";
+import { Header } from "../Header/Header";
 
 const Home = lazy(() =>
   import("../../pages/Home/Home")
@@ -9,23 +10,33 @@ const Search = lazy(() =>
   import("../../pages/Search/Search")
 );
 
+const User = lazy(() =>
+  import("../../pages/User/User")
+);
 const Layouts = () => {
   return (
-    <div>
-      <Suspense fallback="Loading...">
-        <Routes>
-          <Route
-            index
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/search"
-            element={<Search />}
-          />
-          <Route path="*" element={<Navigate to="/" replace />} /> {/*default root */}
-        </Routes>
-      </Suspense>
+    <div className="container-wrapper">
+      <Header />
+      <div className="content-wrapper">
+        <Suspense fallback="Loading...">
+          <Routes>
+            <Route
+              index
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/search"
+              element={<Search />}
+            />
+            <Route
+              path="/user/:username"
+              element={<User />}
+            />
+            <Route path="*" element={<Navigate to="/" replace />} /> {/*default root */}
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 };
